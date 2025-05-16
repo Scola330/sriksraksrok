b458ca97e949513d0860af15bbb2b553aedf0b8a
Your Name <dedude000@gmail.com>
2025-05-16 09:44:31 +0200

    step 13

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import { useState } from 'react';
 
-function Square() {
-  const [value, setValue] = useState(null);
-  function handleClick() {
-    console.log('handleClick function activated');
-    setValue('X');
-    console.log('Value set to x');
-  }
-
-  return (
-    <button
-      className="square"
-      onClick={handleClick}
-    >
-      {value}
-    </button>
-  );
+function Square({ value }) {
+  return <button className="square">{value}</button>;
 }
+
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   return (
