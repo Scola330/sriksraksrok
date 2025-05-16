61e91e40a7af69089781fa49c0c622d518388d9e
Your Name <dedude000@gmail.com>
2025-05-16 09:43:57 +0200

    step 12

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { useState } from 'react';
 function Square() {
   const [value, setValue] = useState(null);
   function handleClick() {
-    alert('handleClick function activated');
+    console.log('handleClick function activated');
     setValue('X');
-    alert('Value set to x');
+    console.log('Value set to x');
   }
 
   return (
@@ -18,22 +18,23 @@ function Square() {
   );
 }
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
